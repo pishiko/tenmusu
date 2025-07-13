304490e1c03e51bf3c5243d3da93c16e47a91521
pishiko <55dorakoro@gmail.com>
2025-07-13 19:01:15 +0900

    parser

--- internal/window/window.go
+++ internal/window/window.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"github.com/pishiko/tenmusu/internal/html"
 	"golang.org/x/text/language"
 )
 
@@ -15,9 +17,9 @@ import (
 var ttf []byte
 
 type Window struct {
-	fontFace *text.GoTextFace
+	fontSource *text.GoTextFaceSource
 
-	text    string
+	tokens  []html.Token
 	scrollY int
 }
 
@@ -36,62 +38,101 @@ func (b *Window) Update() error {
 func (b *Window) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "FPS: "+fmt.Sprintf("%.2f", ebiten.ActualFPS()))
 
+	// pos
 	y := 0 + b.scrollY // Initial Y position with scroll offset
 	x := 0             // Initial X position
-	for _, c := range b.text {
-		w, h := text.Measure(string(c), b.fontFace, 48)
 
-		// 右端まで言ったら改行
-		if x+int(w) > screen.Bounds().Dx() {
-			x = 0
-			y += int(h)
+	//style
+	weight := "normal"
+	style := "roman"
+
+	for _, token := range b.tokens {
+		switch token.Type {
+		case html.Tag:
+			if token.Value == "i" {
+				style = "italic"
+			} else if token.Value == "/i" {
+				style = "roman"
+			} else if token.Value == "b" {
+				weight = "bold"
+			} else if token.Value == "/b" {
+				weight = "normal"
+			}
+		case html.Text:
+			for _, word := range strings.Split(token.Value, " ") {
+				if word == "" {
+					continue // Skip empty words
+				}
+				f := &text.GoTextFace{
+					Source:    b.fontSource,
+					Direction: text.DirectionLeftToRight,
+					Size:      16,
+					Language:  language.Japanese,
+				}
+				w, h := text.Measure(word, f, f.Metrics().HLineGap)
+
+				// 右端まで言ったら改行
+				if x+int(w) > screen.Bounds().Dx() {
+					x = 0
+					y += int(h) //todo fix
+				}
+				// y < 0 はスキップ
+				if y+int(h) < 0 {
+					x += int(w)
+					spaceWidth, _ := text.Measure(" ", f, f.Metrics().HLineGap)
+					x += int(spaceWidth)
+					continue
+				}
+
+				// y > 画面下以降は終了
+				if y > screen.Bounds().Dy() {
+					break
+				}
+
+				// Draw the character
+				op := &text.DrawOptions{}
+
+				if style == "italic" {
+					// todo italic
+					op.GeoM.Skew(-0.5, 0)
+				}
+				if weight == "bold" {
+					// todo bold
+				}
+				op.GeoM.Translate(float64(x), float64(y))
+
+				text.Draw(screen, word, f, op)
+
+				// Update x position for the next character
+				x += int(w)
+				spaceWidth, _ := text.Measure(" ", f, f.Metrics().HLineGap)
+				x += int(spaceWidth)
+			}
 		}
-		// y < 0 はスキップ
-		if y+int(h) < 0 {
-			x += int(w) // Move to the next character
-			continue
-		}
 
-		// y > 画面下以降は終了
-		if y > screen.Bounds().Dy() {
-			break
-		}
-
-		// Draw the character
-		op := &text.DrawOptions{}
-		op.GeoM.Translate(float64(x), float64(y))
-		text.Draw(screen, string(c), b.fontFace, op)
-
-		// Update x position for the next character
-		x += int(w)
 	}
+
 }
 func (b *Window) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return outsideWidth, outsideHeight
 }
 
-func NewWindow(body string) *Window {
+func NewWindow(tokens []html.Token) *Window {
 	f, err := text.NewGoTextFaceSource(bytes.NewReader(ttf))
 	if err != nil {
 		panic(err)
 	}
-	face := &text.GoTextFace{
-		Source:    f,
-		Direction: text.DirectionLeftToRight,
-		Size:      16,
-		Language:  language.Japanese,
-	}
 	return &Window{
-		fontFace: face,
-		text:     body,
-		scrollY:  0,
+		fontSource: f,
+		tokens:     tokens,
+		scrollY:    0,
 	}
 }
 
-func Open(content string) {
+func Open(tokens []html.Token) {
 	ebiten.SetWindowSize(800, 600)
 	ebiten.SetWindowTitle("tenmusu")
-	if err := ebiten.RunGame(NewWindow(content)); err != nil {
+	if err := ebiten.RunGame(NewWindow(tokens)); err != nil {
 		panic(err)
 	}
 	println("Exiting...")
